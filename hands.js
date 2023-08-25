a28faeb6827cafac05f6fe70b02c52553ccdb6ab
Zack Scholl <zack.scholl@gmail.com>
2023-08-25 19:28:15 +0000

    offline

--- hands.js
+++ hands.js
@@ -1,48 +1,19 @@
-import DeviceDetector from "https://cdn.skypack.dev/device-detector-js@2.2.10";
 const mpHands = window;
 const drawingUtils = window;
 const controls = window;
 const controls3d = window;
-// Usage: testSupport({client?: string, os?: string}[])
-// Client and os are regular expressions.
-// See: https://cdn.jsdelivr.net/npm/device-detector-js@2.2.10/README.md for
-// legal values for client and os
-testSupport([
-    { client: 'Chrome' },
-]);
-function testSupport(supportedDevices) {
-    const deviceDetector = new DeviceDetector();
-    const detectedDevice = deviceDetector.parse(navigator.userAgent);
-    let isSupported = false;
-    for (const device of supportedDevices) {
-        if (device.client !== undefined) {
-            const re = new RegExp(`^${device.client}$`);
-            if (!re.test(detectedDevice.client.name)) {
-                continue;
-            }
-        }
-        if (device.os !== undefined) {
-            const re = new RegExp(`^${device.os}$`);
-            if (!re.test(detectedDevice.os.name)) {
-                continue;
-            }
-        }
-        isSupported = true;
-        break;
-    }
-    if (!isSupported) {
-        alert(`This demo, running on ${detectedDevice.client.name}/${detectedDevice.os.name}, ` +
-            `is not well supported at this time, continue at your own risk.`);
-    }
-}
+
 // Our input frames will come from here.
 const videoElement = document.getElementsByClassName('input_video')[0];
 const canvasElement = document.getElementsByClassName('output_canvas')[0];
 const controlsElement = document.getElementsByClassName('control-panel')[0];
 const canvasCtx = canvasElement.getContext('2d');
-const config = { locateFile: (file) => {
-        return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@${mpHands.VERSION}/${file}`;
-    } };
+const config = {
+    locateFile: (file) => {
+        console.log(file);
+        return `/${file}`;
+    }
+};
 // We'll add this to our control panel later, but we'll save it here so we can
 // call tick() each time the graph runs.
 const fpsControl = new controls.FPS();
@@ -51,7 +22,8 @@ const spinner = document.querySelector('.loading');
 spinner.ontransitionend = () => {
     spinner.style.display = 'none';
 };
-const landmarkContainer = document.getElementsByClassName('landmark-grid-container')[0];
+const landmarkContainer =
+    document.getElementsByClassName('landmark-grid-container')[0];
 const grid = new controls3d.LandmarkGrid(landmarkContainer, {
     connectionColor: 0xCCCCCC,
     definedColors: [{ name: 'Left', value: 0xffa500 }, { name: 'Right', value: 0x00ffff }],
@@ -63,6 +35,7 @@ const grid = new controls3d.LandmarkGrid(landmarkContainer, {
     showHidden: false,
     centered: false,
 });
+
 function onResults(results) {
     // Hide the spinner.
     document.body.classList.add('loaded');
@@ -71,13 +44,18 @@ function onResults(results) {
     // Draw the overlays.
     canvasCtx.save();
     canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-    canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+    canvasCtx.drawImage(
+        results.image, 0, 0, canvasElement.width, canvasElement.height);
     if (results.multiHandLandmarks && results.multiHandedness) {
         for (let index = 0; index < results.multiHandLandmarks.length; index++) {
             const classification = results.multiHandedness[index];
             const isRightHand = classification.label === 'Right';
             const landmarks = results.multiHandLandmarks[index];
-            drawingUtils.drawConnectors(canvasCtx, landmarks, mpHands.HAND_CONNECTIONS, { color: isRightHand ? '#00FF00' : '#FF0000' });
+            if (isRightHand && (index == 0 || index == 22)) {
+                console.log(landmarks[1]);
+            }
+            drawingUtils.drawConnectors(
+                canvasCtx, landmarks, mpHands.HAND_CONNECTIONS, { color: isRightHand ? '#00FF00' : '#FF0000' });
             drawingUtils.drawLandmarks(canvasCtx, landmarks, {
                 color: isRightHand ? '#00FF00' : '#FF0000',
                 fillColor: isRightHand ? '#FF0000' : '#00FF00',
@@ -91,12 +69,14 @@ function onResults(results) {
     if (results.multiHandWorldLandmarks) {
         // We only get to call updateLandmarks once, so we need to cook the data to
         // fit. The landmarks just merge, but the connections need to be offset.
-        const landmarks = results.multiHandWorldLandmarks.reduce((prev, current) => [...prev, ...current], []);
+        const landmarks = results.multiHandWorldLandmarks.reduce(
+            (prev, current) => [...prev, ...current], []);
         const colors = [];
         let connections = [];
         for (let loop = 0; loop < results.multiHandWorldLandmarks.length; ++loop) {
             const offset = loop * mpHands.HAND_CONNECTIONS.length;
-            const offsetConnections = mpHands.HAND_CONNECTIONS.map((connection) => [connection[0] + offset, connection[1] + offset]);
+            const offsetConnections = mpHands.HAND_CONNECTIONS.map(
+                (connection) => [connection[0] + offset, connection[1] + offset]);
             connections = connections.concat(offsetConnections);
             const classification = results.multiHandedness[loop];
             colors.push({
@@ -105,8 +85,7 @@ function onResults(results) {
             });
         }
         grid.updateLandmarks(landmarks, connections, colors);
-    }
-    else {
+    } else {
         grid.updateLandmarks([]);
     }
 }
@@ -116,59 +95,58 @@ hands.onResults(onResults);
 // options.
 new controls
     .ControlPanel(controlsElement, {
-    selfieMode: true,
-    maxNumHands: 2,
-    modelComplexity: 1,
-    minDetectionConfidence: 0.5,
-    minTrackingConfidence: 0.5
-})
+        selfieMode: true,
+        maxNumHands: 2,
+        modelComplexity: 1,
+        minDetectionConfidence: 0.5,
+        minTrackingConfidence: 0.5
+    })
     .add([
-    new controls.StaticText({ title: 'MediaPipe Hands' }),
-    fpsControl,
-    new controls.Toggle({ title: 'Selfie Mode', field: 'selfieMode' }),
-    new controls.SourcePicker({
-        onFrame: async (input, size) => {
-            const aspect = size.height / size.width;
-            let width, height;
-            if (window.innerWidth > window.innerHeight) {
-                height = window.innerHeight;
-                width = height / aspect;
-            }
-            else {
-                width = window.innerWidth;
-                height = width * aspect;
-            }
-            canvasElement.width = width;
-            canvasElement.height = height;
-            await hands.send({ image: input });
-        },
-    }),
-    new controls.Slider({
-        title: 'Max Number of Hands',
-        field: 'maxNumHands',
-        range: [1, 4],
-        step: 1
-    }),
-    new controls.Slider({
-        title: 'Model Complexity',
-        field: 'modelComplexity',
-        discrete: ['Lite', 'Full'],
-    }),
-    new controls.Slider({
-        title: 'Min Detection Confidence',
-        field: 'minDetectionConfidence',
-        range: [0, 1],
-        step: 0.01
-    }),
-    new controls.Slider({
-        title: 'Min Tracking Confidence',
-        field: 'minTrackingConfidence',
-        range: [0, 1],
-        step: 0.01
-    }),
-])
+        new controls.StaticText({ title: 'MediaPipe Hands' }),
+        fpsControl,
+        new controls.Toggle({ title: 'Selfie Mode', field: 'selfieMode' }),
+        new controls.SourcePicker({
+            onFrame: async (input, size) => {
+                const aspect = size.height / size.width;
+                let width, height;
+                if (window.innerWidth > window.innerHeight) {
+                    height = window.innerHeight;
+                    width = height / aspect;
+                } else {
+                    width = window.innerWidth;
+                    height = width * aspect;
+                }
+                canvasElement.width = width;
+                canvasElement.height = height;
+                await hands.send({ image: input });
+            },
+        }),
+        new controls.Slider({
+            title: 'Max Number of Hands',
+            field: 'maxNumHands',
+            range: [1, 4],
+            step: 1
+        }),
+        new controls.Slider({
+            title: 'Model Complexity',
+            field: 'modelComplexity',
+            discrete: ['Lite', 'Full'],
+        }),
+        new controls.Slider({
+            title: 'Min Detection Confidence',
+            field: 'minDetectionConfidence',
+            range: [0, 1],
+            step: 0.01
+        }),
+        new controls.Slider({
+            title: 'Min Tracking Confidence',
+            field: 'minTrackingConfidence',
+            range: [0, 1],
+            step: 0.01
+        }),
+    ])
     .on(x => {
-    const options = x;
-    videoElement.classList.toggle('selfie', options.selfieMode);
-    hands.setOptions(options);
-});
+        const options = x;
+        videoElement.classList.toggle('selfie', options.selfieMode);
+        hands.setOptions(options);
+    });
